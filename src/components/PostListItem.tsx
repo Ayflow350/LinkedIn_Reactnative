57472e910f645502d0ad0550d497d97963277572
Ayflow350 <soluwatist@gmail.com>
2023-12-22 19:36:58 +0100

    post list rendered

---
+++ src/components/PostListItem.tsx
@@ -0,0 +1,15 @@
+import {Text} from 'react-native'
+import {Post}  from '../types'
+
+
+type PostListItemProps ={
+   post: Post;
+}
+
+export default  function PostListItem({post}:PostListItemProps){
+   
+    return (
+        <Text>{post.content}</Text>
+    )
+
+}
